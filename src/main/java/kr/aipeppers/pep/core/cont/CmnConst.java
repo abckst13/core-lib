5e177041667f1b2240003d830035dc449f662353
김성태 <java-main@daum.net>
2023-03-10 14:05:29 +0900

    E116 에러코드 추가 - 다른 일자의 출석체크를 진행 할 수 없습니다.

--- src/main/java/kr/aipeppers/pep/core/cont/CmnConst.java
+++ src/main/java/kr/aipeppers/pep/core/cont/CmnConst.java
@@ -142,6 +142,7 @@ public class CmnConst {
 		//	E113	엑셀 오류입니다. - [{0}번째 행, {1}번째 컬럼의 값 {2} : {3}]
 		//	E114	이미 존재하는 데이터 입니다.
 		//	E115	그룹코드 값이 존재하지 않습니다.
+		//  E116	다른 일자의 출석체크를 진행 할 수 없습니다.
 	}
 
 	public static final String AES_ENCRYPT_KEY = "MDEyMzQ1Njc4OUFCQ0RFRg==";  //암호화 키(encrypt key)
