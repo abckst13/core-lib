757f6b67f79ebd121302682b08fd40f368aadc54
김성태 <java-main@daum.net>
2023-03-29 15:31:16 +0900

    return 으로 validation 했던걸 throw exception 으로 처리 커밋

--- src/main/java/kr/aipeppers/pep/core/util/mask/MaskUtil.java
+++ src/main/java/kr/aipeppers/pep/core/util/mask/MaskUtil.java
@@ -1,5 +1,7 @@
 package kr.aipeppers.pep.core.util.mask;
 
+import kr.aipeppers.pep.core.exception.BizException;
+
 public class MaskUtil {
 
     public static String maskPhoneNumber(String phoneNumber) {
@@ -14,19 +16,16 @@ public class MaskUtil {
 
     public static String maskEmail(String email) {
         if (email == null || email.isEmpty()) {
-            return email;
+        	throw new BizException("E107" , new String[] {"이메일이"});  // {0} 존재하지 않습니다.
         }
-
         int index = email.indexOf("@");
         if (index == -1) {
-            return email;
+        	throw new BizException("E112"); // 사용할 수 없는 이메일 입니다.
         }
-
         String emailId = email.substring(0, index);
         if (emailId.length() <= 2) {
-            return email;
+        	throw new BizException("E112"); // 사용할 수 없는 이메일 입니다.
         }
-
         String maskedEmailId = emailId.substring(0, 2) + "*".repeat(emailId.length() - 2);
         String maskedEmail = maskedEmailId + email.substring(index);
         return maskedEmail;
