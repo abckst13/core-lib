2fe0c9f93d594ba718b8cf9b4e1ffc9f62f6d5eb
이성주 <ourage@daum.net>
2023-05-16 15:07:46 +0900

    msgCd 추가및 수정 개선

--- src/main/java/kr/aipeppers/pep/core/domain/ResResultDto.java
+++ src/main/java/kr/aipeppers/pep/core/domain/ResResultDto.java
@@ -12,8 +12,10 @@ import lombok.NoArgsConstructor;
 @ApiModel(value = "REST API 응답 Dto", description = "REST API 응답 Dto")
 @NoArgsConstructor
 public class ResResultDto<T> {
-	@Schema(description = "결과코드")
+	@Schema(description = "성공여부")
 	private String code = "200";
+	@Schema(description = "결과코드")
+	private String msgCd;
 	@Schema(description = "결과메시지")
 	private String msg = "성공";
 	@Schema(description = "결과데이터")
@@ -26,15 +28,17 @@ public class ResResultDto<T> {
 	public ResResultDto(T dto) {
 		this.data = dto;
 	}
-	public ResResultDto(T dto, String code) {
-		if (code.startsWith("I")) {
-			this.msg =  MsgUtil.getMsg(code);
+	public ResResultDto(T dto, String msgCd) {
+		if (msgCd.startsWith("I")) {
+			this.msgCd = msgCd;
+			this.msg =  MsgUtil.getMsg(msgCd);
 		}
 		this.data = dto;
 	}
-	public ResResultDto(T dto, String code, Object[] messageParams) {
-		if (code.startsWith("I")) {
-			this.msg =  MsgUtil.getMsg(code, messageParams);
+	public ResResultDto(T dto, String msgCd, Object[] messageParams) {
+		if (msgCd.startsWith("I")) {
+			this.msgCd = msgCd;
+			this.msg =  MsgUtil.getMsg(msgCd, messageParams);
 		}
 		this.data = dto;
 	}
@@ -44,17 +48,19 @@ public class ResResultDto<T> {
 		this.paginate = paginateDto;
 		this.count = paginateDto.getTotalRecords();
 	}
-	public ResResultDto(T dto, ResPageDto paginateDto, String code) {
-		if (code.startsWith("I")) {
-			this.msg =  MsgUtil.getMsg(code);
+	public ResResultDto(T dto, ResPageDto paginateDto, String msgCd) {
+		if (msgCd.startsWith("I")) {
+			this.msgCd = msgCd;
+			this.msg =  MsgUtil.getMsg(msgCd);
 		}
 		this.data = dto;
 		this.paginate = paginateDto;
 		this.count = paginateDto.getTotalRecords();
 	}
-	public ResResultDto(T dto, ResPageDto paginateDto, String code, Object[] messageParams) {
-		if (code.startsWith("I")) {
-			this.msg =  MsgUtil.getMsg(code, messageParams);
+	public ResResultDto(T dto, ResPageDto paginateDto, String msgCd, Object[] messageParams) {
+		if (msgCd.startsWith("I")) {
+			this.msgCd = msgCd;
+			this.msg =  MsgUtil.getMsg(msgCd, messageParams);
 		}
 		this.data = dto;
 		this.paginate = paginateDto;
