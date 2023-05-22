b4aebbcb565738a6babe0529918b7d283a4582c9
김성태 <java-main@daum.net>
2023-05-22 11:23:18 +0900

    필터 수정 커밋

--- src/main/java/kr/aipeppers/pep/core/util/mask/FilterUtil.java
+++ src/main/java/kr/aipeppers/pep/core/util/mask/FilterUtil.java
@@ -1,27 +1,20 @@
 package kr.aipeppers.pep.core.util.mask;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import kr.aipeppers.pep.core.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class FilterUtil {
 
-	@Autowired
-	SqlSessionTemplate dao;
-
 	/**
 	 * @Method Name : usernameFilter
-	 * @param box
+	 * @param filterText
 	 */
-	public static boolean usernameFilter(String filterText) {
-		String settingText = dao.selectOne("cmn.setFilterView");
-		String[] ArraysStr = settingText.split(",");
-		for(String text : ArraysStr) {
-			if (text.trim().contains(filterText.replaceAll(" ", ""))) { // description 의 내용을 db tb_setting 의 text 내용들과 비교하여 욕설 등 비속어에 속한 단어가 포함되어 있다면
-				throw new BizException("E109"); //E109	유효하지 않은 접근입니다.
+	public static Boolean filterText(String filterText, String filter) {
+		String[] ArraysStr = filter.split(",");
+		for (String text : ArraysStr) {
+			if (text.trim().contains(filterText.replaceAll(" ", ""))) {
+				throw new BizException("E109");
 			}
 		}
 		return true;
